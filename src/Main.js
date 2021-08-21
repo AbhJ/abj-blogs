ca2fb26c3c205754fdbc7c54a7f0a0767c40d639
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2021-08-21 23:10:36 +0530

    Fixed Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,36 +1,21 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import art1 from './pages/art1';
-import art2 from './pages/art2';
-import art3 from './pages/art3';
-// import art4 from './pages/art4';
 import Home from './pages/home';
+import { lazy, Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
 
 const Main = () => {
-
-  const artComponents = {
-    art1: art1,
-    art2: art2,
-    art3: art3,
-  };
-
+  
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/abj-blogs/' component={Home}></Route>
-      
-      {/*
-      <Route exact path='/abj-blogs/blog1' component={art1}></Route>
-      <Route exact path='/abj-blogs/blog2' component={art2}></Route>
-      <Route exact path='/abj-blogs/blog3' component={art3}></Route>
-      */}
-      {
-        [...Array(3).keys()].map((i) => {
-          const artComp = artComponents[`art${i+1}`];
-          return (<Route exact path={`/abj-blogs/blog${i+1}`} component={artComp}></Route>)
-        })
-      }
-
+      <Suspense fallback="">
+        {
+          [...Array(100 /*Number of Blogs*/).keys()].map((i) => {
+            const artComp = lazy(() => import(`./pages/art${i+1}`));
+            return (<Route exact path={`/abj-blogs/blog${i+1}`} key={i+1} component={artComp}></Route>);
+          })
+        }
+      </Suspense>
     </Switch>
   );
 }
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 const Blog = ({ blog }) => {
 	return (
 		<div>
+			<hr />
 			<p>
-				<hr />
 				{blog.id}) {blog.headLine}: <Link to={"/abj-blogs/blog" + blog.id}>
 					link
 				</Link>
