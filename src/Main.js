c9cc41e1290039efab02e6214fdd4674cb1f49ab
abhj <mitraabhijay@gmail.com>
2021-08-22 18:10:41 +0530

    Set number of articles as 10 for faster speed. Have to fix it later though.

--- src/Main.js
+++ src/Main.js
@@ -4,13 +4,13 @@ import { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
 const Main = () => {
-  
+  var length = 10;
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/abj-blogs/' component={Home}></Route>
       <Suspense fallback="">
         {
-          [...Array(100 /*Number of Blogs*/).keys()].map((i) => {
+          [...Array(length /*Number of Blogs*/).keys()].map((i) => {
             const artComp = lazy(() => import(`./pages/art${i+1}`));
             return (<Route exact path={`/abj-blogs/blog${i+1}`} key={i+1} component={artComp}></Route>);
           })
