725aaa4cdcc8b06cc13fc09857a6a916716fe107
abhj <mitraabhijay@gmail.com>
2021-10-10 01:15:11 +0530

    repeat all changes to app.js on index.js to produce complete change on body on theme toggling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Main from './Main';
+import "./syles.css";
 
 // THE MAIN IDEA IS EACH BLOG SHOULD HAVE A LINK.
 // SOME MAY BE EXTERNAL TO OUR SITE.
 
 const App = () => {
+	const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
+	const storedDarkMode = localStorage.getItem("DARK_MODE");
+	const [darkMode, setDarkMode] = useState(storedDarkMode);
+	localStorage.setItem("DARK_MODE", darkMode);
 	return (
-		<div className='App'>
+		<div className='App' data-theme={darkMode ? "dark" : "light"}>
+			<button onClick={toggleDarkMode}>
+  Toggle Dark Mode
+			</button>
+
 			<Header />
 			<Main />
 		</div>
