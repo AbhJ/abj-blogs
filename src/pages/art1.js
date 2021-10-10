0110d40a161cd3f017cc20970c4081e9b2e72b39
abhj <mitraabhijay@gmail.com>
2021-10-10 20:48:52 +0530

    Refined Themes

--- src/pages/art1.js
+++ src/pages/art1.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from "../components/Button"
+import Footer from "../components/Footer"
 
 const art1 = () => {
 	return (
 		<div>
-			<h2>A tribute to Kentaro Miura</h2>
+			<div style={{maxWidth: '90%', paddingLeft: '5%', paddingBottom: '1em', top: '-1em', position: 'relative'}}>
+			<h2>A tribute to Kentaro Miura:</h2>
 
 			<pre>This blog is also available in <a href="https://youtu.be/0gHNucQqVNo">video</a> format.</pre>
 
@@ -14,15 +16,10 @@ const art1 = () => {
 			Kentaro Miura is probably the greatest artist the world has ever seen. If you have read Berserk you would know the effect it has on you, transforming the entirety of your thought process and making you a far more intellectual, smart and empathetic person than you ever were. <br /><br />
 			Being an introvert throughout my life, I appreciate books and literature more than most and being an IITian boosts my understanding of them. I take pride in having lived in the era of Kentaro Miura and do believe that you would feel the same way once you have read even a bit of Berserk. <br /><br />
 			Miura, you will be forever missed.
-
-			<br/><br/>
-			<Button
-				text="To HomePage"
-				className= "toHome"
-				onClick={() => {
-					console.log("tohome clicked");
-				}
-				} />
+			</div>
+		<Footer
+			isBlogsArticle = {true}
+		/>
 		</div>
 	)
 }
