73060b0d0a4492049b3f9c84e8ff93d1c552c482
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2021-11-22 00:43:40 +0530

    Revert "Updates"
    This reverts commit 255f4d95e60659fb156a5880e21980b3085e7a2f.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,9 +5,7 @@ import { Link } from "react-router-dom";
 const Blog = ({ blog }) => {
 	return (
 		<div>
-			{blog.id}) {blog.headLine}: <Link to={"/abj-blogs/blog" + blog.id}>
-				link
-			</Link>
+			{blog.id}) {blog.headLine}:<span className='name-of-blog'> <Link to={"/abj-blogs/blog" + blog.id}>link</Link></span> 
 			<span style={{ float: "right", paddingRight: "1.25em" }} className='div-no-mobile'>
 				created: {blog.day}
 			</span>
