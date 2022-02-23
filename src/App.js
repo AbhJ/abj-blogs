e439b7c979d373953f09b1b8f490c515cc21b3b6
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2022-02-23 12:45:59 +0530

    Cleaned code (resolved linting errrors)

--- src/App.js
+++ src/App.js
@@ -10,31 +10,31 @@ import "./syles.css";
 // SOME MAY BE EXTERNAL TO OUR SITE.
 
 const App = () => {
-	const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
-	const storedDarkMode = localStorage.getItem("DARK_MODE");
-	const [darkMode, setDarkMode] = useState(storedDarkMode);
-	localStorage.setItem("DARK_MODE", darkMode);
-	return (
-		<div className={'App'}>
-			<div className={"Non-footer-stuff"}>
-				<div style={{paddingLeft: '5%'}}>
-					<Button
-						text={darkMode ? "Lit Mode 🌕" : "Dim Mode 🌑"}
-						className={darkMode ? "toggleButtonDar" : "toggleButtonLig"}
-						onClick={() => {
-							toggleDarkMode();
-						}
-						} />
-				</div>
-				<Header />
-				<div className={'Main'} data-theme={darkMode ? "dark" : "light"}>
-					<Main />
-				</div>
-			</div>
-			<Footer />
-			<Copyright />
-		</div>
-	)
+    const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
+    const storedDarkMode = localStorage.getItem("DARK_MODE");
+    const [darkMode, setDarkMode] = useState(storedDarkMode);
+    localStorage.setItem("DARK_MODE", darkMode);
+    return (
+        <div className={'App'}>
+            <div className={"Non-footer-stuff"}>
+                <div style={{paddingLeft: '5%'}}>
+                    <Button
+                        text={darkMode ? "Lit Mode 🌕" : "Dim Mode 🌑"}
+                        className={darkMode ? "toggleButtonDar" : "toggleButtonLig"}
+                        onClick={() => {
+                            toggleDarkMode();
+                        }
+                        } />
+                </div>
+                <Header />
+                <div className={'Main'} data-theme={darkMode ? "dark" : "light"}>
+                    <Main />
+                </div>
+            </div>
+            <Footer />
+            <Copyright />
+        </div>
+    )
 };
 
 export default App;
--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -6,8 +6,6 @@
  */
 
 import React from 'react'
-import PropTypes from 'prop-types'
-import {Link} from "react-router-dom";
 
 const Copyright = ({copyright}) => {
 	return (
