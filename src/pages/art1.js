4459802ae50575a0aab59f086752df9176a3d044
abhj <mitraabhijay@gmail.com>
2022-02-26 22:13:18 +0530

    Updates

--- src/pages/art1.js
+++ src/pages/art1.js
@@ -8,7 +8,7 @@ const art1 = () => {
 				<h2>A Tribute to Kentaro Miura.</h2>
 			<div class="horizontal-rule"></div>
 				<center>
-					<img src='https://upload.wikimedia.org/wikipedia/en/thumb/1/1a/Kentaro_Miura.png/220px-Kentaro_Miura.png' alt='Kentaro Miura' width='60%' />
+					<img src='Kentaro_Miura.png' alt='Kentaro Miura' width='60%' />
 					<br />
 					<pre>Kentaro Miura (source: <a href='https://en.wikipedia.org/wiki/Kentaro_Miura'>Wikipedia</a>)</pre>
 				</center>
