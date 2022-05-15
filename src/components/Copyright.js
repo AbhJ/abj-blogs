490943ed7328cdfadb5ec39420f48c2902d75356
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2022-05-15 16:19:56 +0530

    Updates

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -7,13 +7,22 @@
 
 import React from 'react';
 
+	function getCurrentYear() {
+		return new Date().getFullYear(); // returns the year via local timing
+	};
+
+	function getCurrentMonth() {
+		const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+		return month[new Date().getMonth()]; // returns the year via local timing
+	};
+
 const Copyright = ({copyright}) => {
 	return (
 		<div className={"footer no-select"}>
 			<center>
 				<div className={"copyright text-white"}>
 					<small>
-						Copyright © <a href="https://abhj.github.io">AbJ</a> March 2022
+						Copyright © <a href="https://abhj.github.io">AbJ</a> {getCurrentMonth()} {getCurrentYear()}
 					</small>
 				</div>
 			</center>
